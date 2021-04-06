9a4dc567fa7ed063eb7f594c49f83fe538bfc2f9
richen604 <56615615+richen604@users.noreply.github.com>
2021-04-05 21:12:36 -0700

    Updates

---
+++ component---src-pages-page-2-js-54379e0042a047dcdea6.js
@@ -0,0 +1,2 @@
+(self.webpackChunkrichen_portfolio=self.webpackChunkrichen_portfolio||[]).push([[617],{9863:function(e,t,n){"use strict";n.r(t);var l=n(7294),o=n(5444),c=n(8349),r=n(3751);t.default=function(){return l.createElement(c.Z,null,l.createElement(r.Z,{title:"Page two"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(o.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-54379e0042a047dcdea6.js.map
